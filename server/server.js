95c86cd57306f40768b41204b51d5ecc15b2d42e
GUSTAVO ALVES DOS SANTOS SILVA <galves@totvs.com.br>
2018-06-28 08:35:22 -0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.post("/todos", (req, res)=>{
     });
 
     todo.save().then((doc)=>{
-        res.status(201).json(doc);
+        res.status(200).json(doc);
     }, (err)=>{
         res.status(400).send(err);
     });
@@ -24,5 +24,7 @@ app.post("/todos", (req, res)=>{
 app.listen(3000, ()=>{
     console.log("Started on port 3000");
 })
+
+module.exports = {app}
 
 
