a25792adf95feffbd33fa6990bc626f0d871e2f3
Gustavo Alves dos Santos Silva <gustavoalves.silva97@gmail.com>
2018-09-05 21:23:09 -0300

    Adding PATCH /todos/:id test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -140,3 +140,43 @@ describe("DELETE /todos/:id", ()=>{
             .end(done);
     }) 
 })
+
+describe("PATCH /todos/:id", ()=>{
+    it("should update the todo", (done)=>{
+        var hexId = todos[0]._id.toHexString();
+        var text = "This should be the new text";
+
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed: true,
+            text
+        })
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(text)
+            expect(res.body.todo.completed).toBeTruthy();
+            expect(typeof res.body.todo.completedAt).toBe('number')
+        })
+        .end(done);
+    })
+
+    it("should clear completedAt when todo is not completed", (done)=>{
+        var hexId = todos[1]._id.toHexString();
+        var text = "This should be the new text!!!!";
+
+        request(app)
+        .patch(`/todos/${hexId}`)
+        .send({
+            completed:false,
+            text
+        })
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(text)
+            expect(res.body.todo.completed).toBeFalsy()
+            expect(res.body.todo.completedAt).toBeNull()
+        })
+        .end(done);
+    }) 
+})
